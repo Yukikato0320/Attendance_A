205961d23fe1612665976eefb5da87fc58fad01f
yukikato0320 <yuking0320@gmail.com>
2023-05-15 14:12:25 +0000

    branch_office/model_table_column作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20220722131751) do
+ActiveRecord::Schema.define(version: 20230515135601) do
 
   create_table "attendances", force: :cascade do |t|
     t.date "worked_on"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 20220722131751) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_attendances_on_user_id"
+  end
+
+  create_table "branch_offices", force: :cascade do |t|
+    t.string "branch_office_name"
+    t.string "branch_office_number"
+    t.string "attendance_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
