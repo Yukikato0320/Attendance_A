1e1780573d3de3e95a5121cf823057cc9b337718
yukikato0320 <yuking0320@gmail.com>
2023-05-17 11:05:19 +0000

    branch_officesコントローラー作成

---
+++ app/controllers/branch_offices_controller.rb
@@ -0,0 +1,2 @@
+class BranchOfficesController < ApplicationController
+end
---
+++ test/controllers/branch_offices_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class BranchOfficesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
