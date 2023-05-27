f09713c7042f40eaa391288a2d580ad4fba966b4
yukikato0320 <yuking0320@gmail.com>
2023-05-27 16:09:04 +0000

    拠点ページ作成途中枠組み/20230527

--- app/controllers/branch_offices_controller.rb
+++ app/controllers/branch_offices_controller.rb
@@ -1,9 +1,18 @@
 class BranchOfficesController < ApplicationController
    
   def index
+    @branch_office = BranchOffice.new
+    @branch_offices = BranchOffice.all
   end
   
   def create
+    @branch_office = BranchOffice.create(branch_office_params)
   end
-    
+  
+  private
+  
+  def branch_office_params
+    params.require(:branch_office).permit(:branch_name, :branch_number, :attendance_type)
+  end
+  
 end 
