fa1e96d9b6280eb7d5e5ee360242849a52db81e6
yukikato0320 <yuking0320@gmail.com>
2023-05-28 14:12:25 +0000

    拠点ページview.controller定義/20230528

--- app/controllers/branch_offices_controller.rb
+++ app/controllers/branch_offices_controller.rb
@@ -1,15 +1,51 @@
 class BranchOfficesController < ApplicationController
+  before_action :admin_user
    
   def index
     @branch_office = BranchOffice.new
     @branch_offices = BranchOffice.all
   end
   
-  def create
-    @branch_office = BranchOffice.create(branch_office_params)
+  
+  def new
+    @branch_office = BranchOffice.new
   end
   
+  
+  def create
+    @branch_office = BranchOffice.create(branch_office_params)
+    flash[:success] = "#{@branch_office.branch_office_name}のデータを作成しました。"
+    redirect_to branch_offices_path
+  end
+  
+  
+  def edit
+    @branch_office = BranchOffice.find(params[:id])
+  end
+  
+  
+  def update
+    @branch_office = BranchOffice.find(params[:id])
+    if @branch_office.update(branch_office_params)
+      flash[:success] = '編集に成功しました。'
+      redirect_to branch_office_path
+    else
+      flash[:danger] = '編集に失敗しました。'
+      render :index
+    end
+  end
+  
+  
+  def destroy
+    @branch_office = BranchOffice.find(params[:id])
+    @branch_office.destroy
+    flash[:success] = "#{@branch_office.branch_office_name}のデータを削除しました。"
+    redirect_to branch_offices_path
+  end
+  
+  
   private
+  
   
   def branch_office_params
     params.require(:branch_office).permit(:branch_name, :branch_number, :attendance_type)
