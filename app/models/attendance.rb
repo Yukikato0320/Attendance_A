f3315e1332735e9a520779643587d7c444a2e2d2
Yuki Kato <yuki@YukisMacBook-Pro.local>
2023-09-20 23:06:51 +0900

    Model/attendance/validation編集/2023.09.20

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -6,10 +6,10 @@ class Attendance < ApplicationRecord
   validates :note, length: { maximum: 50 }
   #出勤時間がない場合、退勤時間は無効で、出勤時間を求めるエラーメッセージを出させる。→退勤時間だけだとダメにする。
   validate :finished_at_is_invalid_without_a_started_at
-  #退勤時間がない場合、出勤時間は無効で、退勤時間を求めるエラーメッセージを出させる。→出勤時間だけだとダメにする。
-  validate :started_at_is_invalid_without_a_finished_at, on: :update_one_month
   #出勤・退勤時間どちらも存在する場合、出勤時間より早い退勤時間は無効。→辻褄が合わないとダメにする。
   validate :started_at_than_finished_at_fast_if_invalid
+  #出勤時間と退勤時間両方、+上長を選択・入力しないと無効。
+  validate :all_items_must_be_present_edit_one_month
 
   def finished_at_is_invalid_without_a_started_at
     errors.add(:started_at, "が必要です") if started_at.blank? && finished_at.present?
@@ -24,4 +24,26 @@ class Attendance < ApplicationRecord
       errors.add(:started_at, "より早い退勤時間は無効です") if started_at > finished_at
     end
   end
+
+  def all_items_must_be_present_edit_one_month
+    if (started_at_edited.present? && finished_at_edited.present?) && selector_working_hours_request.blank?
+      errors.add(:started_at_edited, '出社時間と退社時間の両方を入力して、上長を選択してください')
+    end
+    if (started_at_edited.present? && finished_at_edited.blank?) && selector_working_hours_request.blank?
+      errors.add(:started_at_edited, '出社時間と退社時間の両方を入力して、上長を選択してください')
+    end
+    if (started_at_edited.blank? && finished_at_edited.present?) && selector_working_hours_request.blank?
+      errors.add(:started_at_edited, '出社時間と退社時間の両方を入力して、上長を選択してください')
+    end
+    if (started_at_edited.present? && finished_at_edited.present?) && selector_working_hours_request.blank?
+      errors.add(:started_at_edited, '出社時間と退社時間の両方を入力して、上長を選択してください')
+    end
+    if (started_at_edited.present? && finished_at_edited.blank?) && selector_working_hours_request.present?
+      errors.add(:started_at_edited, '出社時間と退社時間の両方を入力して、上長を選択してください')
+    end
+    return unless (started_at_edited.blank? && finished_at_edited.present?) && selector_working_hours_request.present?
+
+    errors.add(:started_at_edited, '出社時間と退社時間の両方を入力して、上長を選択してください')
+  end
+
 end
