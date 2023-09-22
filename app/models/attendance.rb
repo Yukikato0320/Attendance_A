9dd6e6150398c4b4256ee94079232767c5a1c203
Yuki Kato <yuki@YukisMacBook-Pro.local>
2023-09-23 01:47:58 +0900

    Model/attendance.rb/編集/2023.09.22

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -11,20 +11,24 @@ class Attendance < ApplicationRecord
   #出勤時間と退勤時間両方、+上長を選択・入力しないと無効。
   validate :all_items_must_be_present_edit_one_month
 
+# もし出勤時間が、空なら、退勤時間は申請(登録)できない。
   def finished_at_is_invalid_without_a_started_at
     errors.add(:started_at, "が必要です") if started_at.blank? && finished_at.present?
   end
 
-  def started_at_is_invalid_without_a_finished_at
-    errors.add(:finished_at, "が必要です") if finished_at.blank? && started_at.present?
-  end
-
+# もし退勤時間が、出勤時間より早ければ、無効にする。
   def started_at_than_finished_at_fast_if_invalid
-    if started_at.present? && finished_at.present?
-      errors.add(:started_at, "より早い退勤時間は無効です") if started_at > finished_at
+    if next_day_working_hours == false && (started_at.present? && finished_at.present?) && (started_at > finished_at)
+      errors.add(:started_at, "より早い退勤時間は無効です")
     end
+    return unless next_day_working_hours == false
+    return unless started_at_edited.present? && finished_at_edited.present?
+
+    errors.add(:started_at_edited, 'より早い退勤時間は無効です') if started_at_edited > finished_at_edited
   end
 
+
+# 全ての記入がなければ、申請(登録)できない。
   def all_items_must_be_present_edit_one_month
     if (started_at_edited.present? && finished_at_edited.present?) && selector_working_hours_request.blank?
       errors.add(:started_at_edited, '出社時間と退社時間の両方を入力して、上長を選択してください')
@@ -33,9 +37,6 @@ class Attendance < ApplicationRecord
       errors.add(:started_at_edited, '出社時間と退社時間の両方を入力して、上長を選択してください')
     end
     if (started_at_edited.blank? && finished_at_edited.present?) && selector_working_hours_request.blank?
-      errors.add(:started_at_edited, '出社時間と退社時間の両方を入力して、上長を選択してください')
-    end
-    if (started_at_edited.present? && finished_at_edited.present?) && selector_working_hours_request.blank?
       errors.add(:started_at_edited, '出社時間と退社時間の両方を入力して、上長を選択してください')
     end
     if (started_at_edited.present? && finished_at_edited.blank?) && selector_working_hours_request.present?
